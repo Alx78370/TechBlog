ef3f8e6d6a4494612d4f7c63b870b476a9860bce
Alexis VACHET <alexis.vachet@outlook.fr>
2024-11-02 11:31:50 +0100

    1er commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+<h1 class="text-blue-600 text-5xl bg-blue-200 text-center">Hello world</h1>
+  <RouterView />
+</template>
+
